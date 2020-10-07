98da03baf74615d40e723d10f7e87818290ea6f6
USS-Shenzhou <969840795@qq.com>
2020-10-07 22:21:19 +0800

    bug fixed

--- src/main/java/com/ussshenzhou/rainbow6/util/ModItemPropertyOverride.java
+++ src/main/java/com/ussshenzhou/rainbow6/util/ModItemPropertyOverride.java
@@ -18,8 +18,8 @@ public class ModItemPropertyOverride {
     public static void itemPropertyOverride(FMLClientSetupEvent event){
         event.enqueueWork(()->{
             //from BOW's part in ItemModelsProperties.java
-            ItemModelsProperties.registerProperty(ModItems.fragGrenade,new ResourceLocation("pulling"),(p_239428_0_, p_239428_1_, p_239428_2_) -> {
-                return p_239428_2_ != null && p_239428_2_.isHandActive() && p_239428_2_.getActiveItemStack() == p_239428_0_ ? 1.0F : 0.0F;
+            ItemModelsProperties.registerProperty(ModItems.fragGrenade,new ResourceLocation("pulling"),(itemStack, clientWorld, livingEntity) -> {
+                return livingEntity != null && livingEntity.isHandActive() && livingEntity.getActiveItemStack() == itemStack ? 1.0F : 0.0F;
             });
         });
     }
