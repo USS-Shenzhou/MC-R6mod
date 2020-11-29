d85f7d1f3426ef3b0235451826749238501b06fb
USS-Shenzhou <969840795@qq.com>
2020-11-29 20:04:46 +0800

    a temp item for switch between smoke and none

--- src/main/java/com/ussshenzhou/rainbow6/items/CapabilityTestItem.java
+++ src/main/java/com/ussshenzhou/rainbow6/items/CapabilityTestItem.java
@@ -3,12 +3,16 @@ package com.ussshenzhou.rainbow6.items;
 import com.ussshenzhou.rainbow6.capabilities.IR6PlayerCapability;
 import com.ussshenzhou.rainbow6.capabilities.ModCapabilities;
 import com.ussshenzhou.rainbow6.util.ModItemGroups;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.LazyOptional;
 import org.apache.logging.log4j.LogManager;
@@ -27,10 +31,15 @@ public class CapabilityTestItem extends Item {
 
         if (!worldIn.isRemote){
             LazyOptional<IR6PlayerCapability> r6PlayerCap = playerIn.getCapability(ModCapabilities.R6_PLAYER_CAPABILITY);
-            r6PlayerCap.ifPresent((cap)->{
-                        cap.setOperator("smoke");
-                    }
-            );
+            IR6PlayerCapability ir6PlayerCapability = r6PlayerCap.orElse(ModCapabilities.R6_PLAYER_CAPABILITY.getDefaultInstance());
+            if ("smoke".equals(ir6PlayerCapability.getOperator())){
+                ir6PlayerCapability.setOperator("none");
+                playerIn.sendMessage(new StringTextComponent("Successfully switch smoke to none"),playerIn.getUniqueID());
+            }
+            else{
+                ir6PlayerCapability.setOperator("smoke");
+                playerIn.sendMessage(new StringTextComponent("Successfully switch none to smoke"),playerIn.getUniqueID());
+            }
         }
         return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
     }
